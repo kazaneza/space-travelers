7751971e0cf47f85b6a873325f715dbe8c1de4ea
kazaneza <kazanezagentil@gmail.com>
2023-03-24 07:44:38 +0100

    Fetch data from API

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setMissions, selectMissions } from '../features/missions/missionSlice';
 
-const Missions = () => (
-  <div>
-    <h1> Missions </h1>
-    <p>This is the missions page</p>
-  </div>
-);
+const Missions = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector(selectMissions);
+
+  useEffect(() => {
+    const fetchMissions = async () => {
+      const response = await fetch('https://api.spacexdata.com/v3/missions');
+      const data = await response.json();
+      dispatch(setMissions(data));
+    };
+
+    if (missions.length === 0) {
+      fetchMissions();
+    }
+  }, [missions, dispatch]);
+
+  return (
+    <div>
+      <h1>Missions</h1>
+      {missions.map((mission) => (
+        <div key={mission.mission_id}>
+          <h2>{mission.mission_name}</h2>
+          <p>{mission.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Missions;
