b0320d705c9203640a591a732a4e2ceca3afd6b1
kazaneza <kazanezagentil@gmail.com>
2023-03-24 08:15:39 +0100

    import bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import AppWithProvider from './features/store';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
