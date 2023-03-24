c015d832e5011c1b6f2156c83c23d61b8c72d3e3
kazaneza <kazanezagentil@gmail.com>
2023-03-24 08:16:32 +0100

    display data

--- src/features/missions/missionSlice.js
+++ src/features/missions/missionSlice.js
@@ -1,7 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
   missions: [],
+  status: 'idle',
+  error: null,
 };
 
 export const missionSlice = createSlice({
@@ -9,13 +12,34 @@ export const missionSlice = createSlice({
   initialState,
   reducers: {
     setMissions: (state, action) => {
-      state.missions = action.payload; // esli
+      state.missions = action.payload;
+    },
+    setMissionsStatus: (state, action) => {
+      state.status = action.payload;
+    },
+    setMissionsError: (state, action) => {
+      state.error = action.payload;
     },
   },
 });
 
-export const { setMissions } = missionSlice.actions;
+export const { setMissions, setMissionsStatus, setMissionsError } = missionSlice.actions;
+
+export const fetchMissions = () => async (dispatch) => {
+  dispatch(setMissionsStatus('loading'));
+
+  try {
+    const response = await axios.get('https://api.spacexdata.com/v3/missions');
+    dispatch(setMissions(response.data));
+    dispatch(setMissionsStatus('succeeded'));
+  } catch (error) {
+    dispatch(setMissionsError(error.message));
+    dispatch(setMissionsStatus('failed'));
+  }
+};
 
 export const selectMissions = (state) => state.missions.missions;
+export const selectMissionsStatus = (state) => state.missions.status;
+export const selectMissionsError = (state) => state.missions.error;
 
 export default missionSlice.reducer;
